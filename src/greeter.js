d8b528e93885af8d7d185372515637573ca5edc0
Jessyka Lage <jessykalage@gmail.com>
2020-07-01 18:19:32 -0300

    greeter kata: 3 condition

--- src/greeter.js
+++ src/greeter.js
@@ -1,4 +1,7 @@
-const greet = name => `Hello ${name.trim()}`
+const greet = name => {
+    name = name.trim();
+    return `Hello ${name.charAt(0).toUpperCase() + name.slice(1)}`
+}
 
 module.exports = {
     greet
--- tests/greeter.test.js
+++ tests/greeter.test.js
@@ -6,6 +6,10 @@ describe('Greeter Class', () => {
     })
 
     it('receives a name as input, trims the input and outputs Hello <name>.', () => {
-        expect(greet('Joao Paulo  ')).toBe('Hello Joao Paulo')
+        expect(greet('Joao  ')).toBe('Hello Joao')
+    })
+
+    it('receives a name as input, capitalizes the first letter of the name and outputs Hello <name>.', () => {
+        expect(greet('joao')).toBe('Hello Joao')
     })
 })

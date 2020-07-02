504fd9c8b24ee7ddbb191467381c6d1e11b5de16
Jessyka Lage <jessykalage@gmail.com>
2020-07-02 20:39:27 -0300

    kata stringCalculator: steps 2 - 5

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,4 +1,13 @@
-const sumOfNumbers = myString => 0
+const sumOfNumbers = myString => {
+  if(myString == '')
+    return 0;
+  else{
+    let number = myString.split( /\s*,|\s\n*/);
+    let sum = 0;
+    number.forEach(item => sum += parseInt(item))
+    return sum;
+  }
+}
 
 module.exports = {
   sumOfNumbers
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -4,4 +4,20 @@ describe('String Calculator', () => {
   it('returns 0 when empty string', () => {
     expect(sumOfNumbers('')).toBe(0);
   })
+
+  it('retuns the value when a single number', () => {
+    expect(sumOfNumbers('5')).toBe(5);
+  })
+
+  it('returns the sum when two numbers, comma delimited', () => {
+    expect(sumOfNumbers('10,2')).toBe(12);
+  })
+  
+  it('returns the sum when two numbers, newline delimited', () => {
+    expect(sumOfNumbers('1\n2')).toBe(3);
+  })
+  
+  it('returns the sum when three numbers,  delimited either way', () => {
+    expect(sumOfNumbers('1\n2,3\n4')).toBe(10);
+  })
 })

00af4ae05b7d267a863589fd6078f2f748e515d1
Jessyka Lage <jessykalage@gmail.com>
2020-07-02 21:19:02 -0300

    kata stringCalculator: refactor code

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -4,12 +4,11 @@ const sumOfNumbers = myString => {
   let sum = 0;
   let negativeNumbers = '';
   if(myString !== ''){;
-    let number = getStringNumber(myString); 
-    number.forEach(item => {
+    const numbers = getNumbersFromString(myString); 
+    numbers.forEach(item => {
       let currentNumber = parseInt(item);
-      if(currentNumber < 0){
+      if(currentNumber < 0)
         negativeNumbers += negativeNumbers == '' ? item : `,${item}`
-      }
       else
         sum += currentNumber >= 1000 ? 0 : currentNumber;
     })
@@ -17,15 +16,13 @@ const sumOfNumbers = myString => {
   return negativeNumbers ? `${ERROR_MESSAGE}: ${negativeNumbers}` : sum;
 }
 
-const getStringNumber = myString => {
-  let number;
+const getNumbersFromString = myString => {
+  let newDelimiter = '';
   if(myString.slice(0,2) == '//'){
-    const newDelimiter = myString.charAt(2);
-    number = myString.slice(4).split(new RegExp(`[,${newDelimiter}\n]`, 'g'));
-  }else{
-    number = myString.split( /\s*,|\s\n*/); 
+    newDelimiter = `${myString.charAt(2)}`;
+    myString = myString.slice(4);
   }
-  return number;
+  return myString.split(new RegExp(`[,${newDelimiter}\n]`, 'g'));;
 }
 
 module.exports = {

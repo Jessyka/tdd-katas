3ddfdddae36abe272432a7f78770509481aa93e6
Jessyka Lage <jessykalage@gmail.com>
2020-07-02 21:20:34 -0300

    kata stringCalculator: refactor code

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,15 +3,15 @@ const ERROR_MESSAGE = 'negatives not allowed';
 const sumOfNumbers = myString => {
   let sum = 0;
   let negativeNumbers = '';
-  if(myString !== ''){;
+  if(myString !== ''){
     const numbers = getNumbersFromString(myString); 
     numbers.forEach(item => {
       let currentNumber = parseInt(item);
       if(currentNumber < 0)
-        negativeNumbers += negativeNumbers == '' ? item : `,${item}`
+        negativeNumbers += negativeNumbers == '' ? item : `,${item}`;
       else
         sum += currentNumber >= 1000 ? 0 : currentNumber;
-    })
+    });
   }
   return negativeNumbers ? `${ERROR_MESSAGE}: ${negativeNumbers}` : sum;
 }
@@ -22,7 +22,7 @@ const getNumbersFromString = myString => {
     newDelimiter = `${myString.charAt(2)}`;
     myString = myString.slice(4);
   }
-  return myString.split(new RegExp(`[,${newDelimiter}\n]`, 'g'));;
+  return myString.split(new RegExp(`[,${newDelimiter}\n]`, 'g'));
 }
 
 module.exports = {
